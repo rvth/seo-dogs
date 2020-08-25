85c88dcb2e1c8bc21b7af453d509ddc9c76d0435
Ruth <ruthx27@gmail.com>
2020-08-25 21:10:20 +0100

    new pages

--- main.py
+++ main.py
@@ -10,5 +10,30 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route("/crawling-and-indexing")
+def crawling():
+    return render_template("crawling-and-indexing.html")
+
+@app.route("/site-structure")
+def structure():
+    return render_template("site-structure.html")
+
+@app.route("/on-page")
+def onpage():
+    return render_template("on-page.html")
+
+@app.route("/user-experience")
+def users():
+    return render_template("user-experience.html")
+
+@app.route("/links")
+def links():
+    return render_template("links.html")
+
+@app.route("/international-seo")
+def international():
+    return render_template("international-seo.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
